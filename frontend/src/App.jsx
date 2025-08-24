4aaac8a66b07da673f569d28fc85a50f84dad55b
jinjie-hub <jinjie043@gmail.com>
2025-08-24 13:40:34 +0800

    main page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,92 @@
-import React from "react";
+import React, { useState } from 'react'; // <-- Must include this
+import { ColorModeButton } from "./components/ui/color-mode"
+import { Avatar, For, HStack, IconButton, Input, CloseButton, Group } from "@chakra-ui/react"
+import { LuSearch } from "react-icons/lu"
+
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
 
-export default App
+  const [showSearch, setShowSearch] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const toggleSearch = () => {
+    setShowSearch(!showSearch);
+    setSearchQuery("");
+  };
+
+
+
+  return (
+    <>
+    <HStack gap="10">
+
+      {/* Hearder for all pages */}
+      <header>
+        <nav>
+          <a className="logo" href="#home">Logo</a>
+          <a href="#about">About</a>
+          <a href="#services">Services</a>
+          <a href="#contact">Contact</a>
+          <a href="about_us">About us</a>
+        </nav>
+
+      
+      <div className="header-right">
+
+        {!showSearch && (
+          <IconButton
+              aria-label="Call support"
+              key={"ghost"}
+              variant={"ghost"}
+              onClick={toggleSearch}
+            >
+          <LuSearch />
+        </IconButton>
+        )}
+
+        {showSearch && (
+          <Group>
+              <Input
+                flex="1"
+                width="500px"
+                placeholder="Search..."
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                
+              />
+
+              <IconButton
+
+                key={"ghost"}
+                variant={"ghost"}
+              >
+                <LuSearch />
+              </IconButton>
+
+              <CloseButton 
+                variant="ghost"
+                onClick={toggleSearch}
+              />
+          </Group>
+        )}
+
+        {/* Dark Mode Toggle Button */}
+        <ColorModeButton className="dark-mode-btn" />
+
+        {/* Header for user login */}
+        <a href="user_profile">
+        <Avatar.Root colorPalette="grey">
+          <Avatar.Fallback />
+          <Avatar.Image src="https://bit.ly/broken-link" />
+        </Avatar.Root>
+        </a>
+
+      </div>
+      </header>
+
+    </HStack>
+    </>
+  );
+};
+
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,17 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { ColorModeProvider } from "./components/ui/color-mode"
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
-import { ColorModeButton } from "./components/ui/color-mode"
+import { ColorModeProvider } from "./components/ui/color-mode"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ChakraProvider value={defaultSystem}>
-       <ColorModeProvider>
-          <ColorModeButton className="dark-mode-btn"/>
-          <App />
-       </ColorModeProvider>
+      <ColorModeProvider>
+        <App />
+      </ColorModeProvider>
     </ChakraProvider>
   </StrictMode>,
 )
